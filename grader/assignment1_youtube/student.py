b7f18bf57cc2415484beecc1d10259db0d527abf
Sukrit Chanachaimongkonkun <tonwan.sukrit@gmail.com>
2025-01-13 11:16:31 +0700

    finish problem 2

--- grader/assignment1_youtube/student.py
+++ grader/assignment1_youtube/student.py
@@ -11,8 +11,14 @@ def Q1():
         1. How many rows are there in the GBvideos.csv after removing duplications?
         - To access 'GBvideos.csv', use the path '/data/GBvideos.csv'.
     """
-    # TODO: Paste your code here
-    return None
+    file = '/data/GBvideos.csv'
+    vdo_df = pd.read_csv(file)
+
+    # Drop duplicates
+    # inplace=True will modify the DataFrame in place rather than returning a new DataFrame.
+    vdo_df.drop_duplicates(inplace=True)
+
+    return vdo_df.count()[0]
 
 def Q2(vdo_df):
     '''
@@ -20,8 +26,11 @@ def Q2(vdo_df):
             - GBvideos.csv has been loaded into memory and is ready to be utilized as vdo_df
             - The duplicate rows of vdo_df have been removed.
     '''
-    # TODO: Paste your code here
-    return None
+    condition = vdo_df['dislikes'] > vdo_df['likes']
+
+    # vdo_df[condition][0]
+    # nunique() is used to count the number of unique values in a Series.
+    return vdo_df[condition]['title'].nunique()
 
 def Q3(vdo_df):
     '''
@@ -30,8 +39,9 @@ def Q3(vdo_df):
             - The duplicate rows of vdo_df have been removed.
             - The trending date of vdo_df is represented as 'YY.DD.MM'. For example, January 22, 2018, is represented as '18.22.01'.
     '''
-    # TODO: Paste your code here
-    return None
+   
+    condition = (vdo_df['trending_date'] == '18.22.01') & (vdo_df['comment_count'] > 10000)
+    return vdo_df[condition].count()[0]
 
 def Q4(vdo_df):
     '''
@@ -39,8 +49,10 @@ def Q4(vdo_df):
             - GBvideos.csv has been loaded into memory and is ready to be utilized as vdo_df
             - The duplicate rows of vdo_df have been removed.
     '''
-    # TODO:  Paste your code here
-    return None
+    # vdo_df.groupby('trending_date')['comment_count'].mean().idxmin()
+
+    # idxmin() is used to get the index of the first minimum value.
+    return vdo_df.groupby('trending_date')['comment_count'].mean().idxmin() 
 
 def Q5(vdo_df):
     '''
@@ -51,4 +63,29 @@ def Q5(vdo_df):
             - To access 'GB_category_id.json', use the path '/data/GB_category_id.json'.
     '''
     # TODO:  Paste your code here
-    return None
+
+    jn = '/data/GB_category_id.json'
+    with open(jn) as f:
+        data = json.load(f)
+    
+    cat_list = []
+    for d in data['items']:
+        cat_list.append((int(d['id']), d['snippet']['title'])) 
+
+    cat_df = pd.DataFrame(cat_list, columns=['id', 'category'])
+
+    # print(cat_df.head(1))
+    vdo_df_withcat = vdo_df.merge(cat_df, left_on='category_id', right_on='id')
+
+    vdp_df_withcat = vdo_df_withcat.groupby(['trending_date', 'category'])['views'].sum()
+
+    cnt = 0
+    for a in vdp_df_withcat:
+        # print(a[1].keys())
+        # print(a[1][a[0]]['Sports'])
+        # print(a[1].values())
+        if(a[1][a[0]]['Sports'] > a[1][a[0]]['Comedy']):
+            cnt += 1
+
+
+    return cnt
---
+++ grader/assignment1_youtube/test.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import json
+
+file = 'USvideos.csv'
+vdo_df = pd.read_csv(file)
+
+# print(vdo_df.count()[0])
+vdo_df.drop_duplicates(inplace=True)
+
+condition = (vdo_df['trending_date'] == '18.22.01') & (vdo_df['comment_count'] > 10000)
+
+
+# Q3
+print(vdo_df[condition].count()[0])
+
+
+# Q4
+print(vdo_df.groupby('trending_date')['comment_count'].mean().idxmin())
+
+
+# Q5
+
+
+jn = 'US_category_id.json'
+with open(jn) as f:
+    data = json.load(f)
+
+cat_list = []
+for d in data['items']:
+   cat_list.append((int(d['id']), d['snippet']['title'])) 
+
+cat_df = pd.DataFrame(cat_list, columns=['id', 'category'])
+
+# print(cat_df.head(1))
+vdo_df_withcat = vdo_df.merge(cat_df, left_on='category_id', right_on='id')
+
+vdp_df_withcat = vdo_df_withcat.groupby(['trending_date', 'category'])['views'].sum()
+
+print(vdp_df_withcat.head(5))
+print(";----------'")
+
+vdp_df_withcat = vdp_df_withcat.groupby(['trending_date'])
+
+print(vdp_df_withcat.head(5))
+
+cnt = 0
+for a in vdp_df_withcat:
+    # print(a[1].keys())
+    # print(a[1][a[0]]['Sports'])
+    # print(a[1].values())
+    if(a[1][a[0]]['Sports'] > a[1][a[0]]['Comedy']):
+        cnt += 1
+
+print(cnt)
