06cbd8441a79f2a67a4fef339ca06fdcdd036957
Sukrit Chanachaimongkonkun <tonwan.sukrit@gmail.com>
2025-01-15 21:22:22 +0700

    week 2 assignment

---
+++ grader/assignment2_dataprep/student.py
@@ -0,0 +1,92 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+"""
+    ASSIGNMENT 2 (STUDENT VERSION):
+    Using pandas to explore Titanic data from Kaggle (titanic_to_student.csv) and answer the questions.
+    (Note that the following functions already take the Titanic dataset as a DataFrame, so you don’t need to use read_csv.)
+
+"""
+
+
+def Q1(df):
+    """
+        Problem 1:
+            How many rows are there in the "titanic_to_student.csv"?
+    """
+    # TODO: Code here
+    
+    return None
+
+
+def Q2(df):
+    '''
+        Problem 2:
+            Drop unqualified variables
+            Drop variables with missing > 50%
+            Drop categorical variables with flat values > 70% (variables with the same value in the same column)
+            How many columns do we have left?
+    '''
+    # TODO: Code here
+    return None
+
+
+def Q3(df):
+    '''
+       Problem 3:
+            Remove all rows with missing targets (the variable "Survived")
+            How many rows do we have left?
+    '''
+    # TODO: Code here
+    return None
+
+
+def Q4(df):
+    '''
+       Problem 4:
+            Handle outliers
+            For the variable “Fare”, replace outlier values with the boundary values
+            If value < (Q1 - 1.5IQR), replace with (Q1 - 1.5IQR)
+            If value > (Q3 + 1.5IQR), replace with (Q3 + 1.5IQR)
+            What is the mean of “Fare” after replacing the outliers (round 2 decimal points)?
+            Hint: Use function round(_, 2)
+    '''
+    # TODO: Code here
+    return None
+
+
+def Q5(df):
+    '''
+       Problem 5:
+            Impute missing value
+            For number type column, impute missing values with mean
+            What is the average (mean) of “Age” after imputing the missing values (round 2 decimal points)?
+            Hint: Use function round(_, 2)
+    '''
+    # TODO: Code here
+    return None
+
+
+def Q6(df):
+    '''
+        Problem 6:
+            Convert categorical to numeric values
+            For the variable “Embarked”, perform the dummy coding.
+            What is the average (mean) of “Embarked_Q” after performing dummy coding (round 2 decimal points)?
+            Hint: Use function round(_, 2)
+    '''
+    # TODO: Code here
+    return None
+
+
+def Q7(df):
+    '''
+        Problem 7:
+            Split train/test split with stratification using 70%:30% and random seed with 123
+            Show a proportion between survived (1) and died (0) in all data sets (total data, train, test)
+            What is the proportion of survivors (survived = 1) in the training data (round 2 decimal points)?
+            Hint: Use function round(_, 2), and train_test_split() from sklearn.model_selection, 
+            Don't forget to impute missing values with mean.
+    '''
+    # TODO: Code here
+    return None
